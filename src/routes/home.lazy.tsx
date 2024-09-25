e435fb169648fda126d3b0354f77e2cb7d40185a
Johan Sandgren <gussanjoba@student.gu.se>
2024-09-25 18:06:32 +0200

    Refactor home.lazy.tsx to improve layout structure and readability

--- src/routes/home.lazy.tsx
+++ src/routes/home.lazy.tsx
@@ -8,22 +8,30 @@ export const Route = createLazyFileRoute('/home')({
 const Dashboard = () => {
   return (
     <div className="flex flex-1">
+
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex gap-2">
-          {[...new Array(4)].map((i) => (
-            <div
-              key={"first-array" + i}
-              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
+
+          <div
+            key={"first-array"}
+            className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+          ></div>
+
         </div>
         <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((i) => (
-            <div
-              key={"second-array" + i}
-              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
+
+          {/* Left part: 80% of the width */}
+          <div
+            key={"left-section"}
+            className="h-full w-full flex-[4] rounded-lg bg-gray-100 dark:bg-neutral-800 animate-pulse"
+          ></div>
+
+          {/* Right part: 20% of the width */}
+          <div
+            key={"right-section"}
+            className="h-full w-full flex-[1] rounded-lg bg-gray-100 dark:bg-neutral-800 animate-pulse"
+          ></div>
+
         </div>
       </div>
     </div>
